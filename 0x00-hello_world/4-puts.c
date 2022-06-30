fafd54ad9b8b2c30fd26faaf18f2517376c0e9e8
Unuakpa Akpevweoghene <naomieloor@gmail.com>
2022-06-30 13:39:19 -0700

    Task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+* main - Entry point
+* Description: Prints the description specified
+* Return: always zero(sucess)
+* /
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
