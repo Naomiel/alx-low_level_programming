f5e622df7bf73d7506f5778797235377d60d60a2
Unuakpa Akpevweoghene <naomieloor@gmail.com>
2022-07-07 14:29:04 -0700

    Task 2

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,15 +1,12 @@
-#include "main.h"
-/**print_alphabet - function to *print abc
-*
-* Return: 0
-*/
-void print_alphabet(void)
-{ 
-char c;
-
-for (c = 'a'; c <= 'z; c++
-}
-_putchar(c); 
-} 
-_putchar ('\n');
-}
+#ifndef MAIN_H 
+ 
+#define MAIN_H 
+ 
+#include <stdio.h> 
+ 
+void positive_or_negative(int i); 
+int largest_number(int a, int b, int c); 
+int convert_day(int month, int day); 
+void print_remaining_days(int month, int day, int year); 
+ 
+#endif /* MAIN_H */
